b56bcc0e57f825b992b4fec545dc4e10924b85ec
JoshuaGoldberg <56654967+JoshuaGoldberg@users.noreply.github.com>
2023-02-27 18:44:40 -0500

    base stuff

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,27 @@
+package frc.robot;
+
+import edu.wpi.first.math.util.Units;
+
+public class Constants {
+    public static final int zero = 0; // in case you need a zero :)
+    public static final int frontLeftMotorPort = 1;
+    public static final int backLeftMotorPort = 2;
+    public static final int frontRightMotorPort = 3;
+    public static final int backRightMotorPort = 4;
+
+  public static final class DriveConstants {
+    public static final double kJoystickTurnDeadzone = 0.04;
+    public static final double wheelDiameter = 5;
+    public static final double gearRatio = 60.0 / 11.0 * 28.0 / 20; // 60.0 / 10.0;
+    public static final double distPerPulse =
+        (1.0 / gearRatio) * Units.inchesToMeters(wheelDiameter) * Math.PI;
+    public static final int currentLimit = 65;
+    private static final double bumperlessRobotLength = Units.inchesToMeters(26);
+    private static final double bumperlessRobotWidth = Units.inchesToMeters(24);
+    private static final double bumperThickness = Units.inchesToMeters(3);
+    public static final double fullRobotWidth = bumperlessRobotWidth + bumperThickness * 2;
+    public static final double fullRobotLength = bumperlessRobotLength + bumperThickness * 2;
+ }
+
+    
+}
---
+++ src/main/java/frc/robot/DriveSubsystem.java
@@ -0,0 +1,20 @@
+package frc.robot;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMax.IdleMode;
+import com.revrobotics.CANSparkMaxLowLevel;
+import com.revrobotics.RelativeEncoder;
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
+import edu.wpi.first.math.kinematics.DifferentialDriveWheelSpeeds;
+import edu.wpi.first.wpilibj.ADXRS450_Gyro;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+import frc.robot.Constants.DriveConstants;
+
+public class DriveSubsystem {
+    
+}
